d26967d1793f5b6e85118a2daff0cdace7654e1d
Mai Phi Hieu <mphieu2003@gmail.com>
2025-06-20 16:20:40 +0700

    feat: create POST /tasks endpoint

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const tasks = [
     { id: 2, name: 'di cho mua thuc pham cuoi tuan', complated: true },
     { id: 3, name: `Doc het chuong 5 cua sanh "cleancode"`, complated: false },
 ]
-const app = express()
 
+const app = express()
+app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Server is running on port 3000')
 })
@@ -19,5 +20,23 @@ app.get('/api/v1/tasks', (req, res) => {
         }
     });
 })
+
+app.post('/api/v1/tasks', (req, res) => {
+    const newID = tasks.length > 0 ? tasks[tasks.length - 1].id + 1 : 1;
+    const newTask ={
+        id: newID,
+        name:req.body.name,
+        complated: false
+    };
+
+    tasks.push(newTask);
+
+    res.status(201).json({
+        status:'success',
+        data:{
+            task: newTask
+        }
+    });
+});
 
 app.listen(3000)
